4f30bea687e30f67ab352424548582880ef99a9a
cassialeaal <cassialeaal@gmail.com>
2021-08-06 19:13:54 -0300

    comandos basicos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+var d = new Date();
+alert(d.getMonth());
+
+/*var count;
+for(count =0; count <=5 ; count++){
+    alert(count);
+}*/
+
+/*
+var count = 0;
+while(count<5){
+    console.log(count);
+    count++;
+}*/
+
+/*
+var idade = prompt("Qual sua idade");
+if(idade < 18){
+    alert("Menor de idade");
+}
+else{
+    alert("Maior de idade");
+}*/
+
+
+/*var fruta = [{nome: "maça", cor:"vermelha"}, {nome:"uva", cor: "roxo"}]
+console.log(fruta);
+alert(fruta[1].nome);
+*/
+/*
+var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
 /*var nome = "Cassia Leal";
 var idade = 24;
 var idade2 = 10;
@@ -10,6 +45,9 @@ console.log(idade + n);
 //console.log(frase.toUpperCase());
 alert(frase.replace("Japão","Brasil"));
 */
-var lista = ["maça", "pera","laranja"];
-lista.push("uva");
-console.log(lista);
+//var lista = ["maça", "pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
