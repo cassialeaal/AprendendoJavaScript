1ca1bcedb4ec32549a029066b53e82c8186224a4
cassialeaal <cassialeaal@gmail.com>
2021-08-05 23:03:48 -0300

    Aprendendo comandos básicos

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Cassia Leal";
+var idade = 24;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem" + idade + " anos");
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + n);
+//console.log(frase.toUpperCase());
+alert(frase.replace("Japão","Brasil"));
